d69dd05241571adeb6ae359b641f65b77a26955e
Somya Ranjan Khatua <115935107+somyaranjan007@users.noreply.github.com>
2024-08-08 04:05:33 -0700

    creating UserBasket.sol

--- contract/src/BasketFactory.sol
+++ contract/src/BasketFactory.sol
@@ -4,52 +4,68 @@ pragma solidity ^0.8.13;
 import "./BasketToken.sol";
 import "./interface/IBasketToken.sol";
 import "./interface/IConstant.sol";
+import "./UserBaskets.sol";
 
 contract BasketFactory {
     address[] public allBasket;
+    address[] public allBasketOfBasket;
 
     event CreateBasket(
         address indexed creator,
         address indexed basketAddress,
         string name,
         string symbol,
-        IConstant.TokenInfo[] tokens
+        IConstant.BasketInfo[] tokens
+    );
+
+    event CreateBasketOfBasket(
+        address indexed creator,
+        address indexed basketOfBasketAddress,
+        string name,
+        string symbol,
+        IConstant.BasketInfo[] baskets
     );
 
     function createBasket(
         string memory name,
         string memory symbol,
-        IConstant.TokenInfo[] memory tokens
+        IConstant.BasketInfo[] memory tokens,
+        bool isCreateBasketToken
     ) public payable {
-        require(msg.value >= 0.01 ether, "minimum ETH!");
         address basket;
-        bytes memory bytecode = getBytecode(name, symbol, tokens);
+        bytes memory bytecode = getBytecode(
+            name,
+            symbol,
+            tokens,
+            isCreateBasketToken
+        );
         bytes32 salt = createHash(msg.sender, name, symbol, tokens);
         assembly {
             basket := create2(0, add(bytecode, 0x20), mload(bytecode), salt)
         }
-        bool success = IBasketToken(basket).depositBasketToken{
-            value: msg.value
-        }(msg.sender);
-        require(success, "Fund tokens failed");
-        allBasket.push(basket);
-        emit CreateBasket(msg.sender, basket, name, symbol, tokens);
+        if (isCreateBasketToken) {
+            require(msg.value >= 0.01 ether, "minimum ETH!");
+            bool success = IBasketToken(basket).depositBasketToken{
+                value: msg.value
+            }(msg.sender);
+            require(success, "Fund tokens failed");
+            allBasket.push(basket);
+            emit CreateBasket(msg.sender, basket, name, symbol, tokens);
+        } else {
+            allBasketOfBasket.push(basket);
+            emit CreateBasketOfBasket(msg.sender, basket, name, symbol, tokens);
+        }
     }
 
     function createHash(
         address owner,
         string memory name,
         string memory symbol,
-        IConstant.TokenInfo[] memory tokens
+        IConstant.BasketInfo[] memory tokens
     ) internal pure returns (bytes32) {
         bytes memory data = abi.encodePacked(name, symbol, owner);
         for (uint256 i = 0; i < tokens.length; i++) {
-            data = abi.encodePacked(
-                data,
-                tokens[i].addr,
-                tokens[i].name,
-                tokens[i].percent
-            );
+            data = abi.encodePacked(data, tokens[i].addr, tokens[i].percent);
         }
         return keccak256(data);
     }
@@ -57,9 +73,15 @@ contract BasketFactory {
     function getBytecode(
         string memory name,
         string memory symbol,
-        IConstant.TokenInfo[] memory tokens
+        IConstant.BasketInfo[] memory tokens,
+        bool isCreateBasketToken
     ) internal pure returns (bytes memory) {
-        bytes memory bytecode = type(ERC7621).creationCode;
+        bytes memory bytecode;
+        if (isCreateBasketToken) {
+            bytecode = type(ERC7621).creationCode;
+        } else {
+            bytecode = type(UserBasket).creationCode;
+        }
         return abi.encodePacked(bytecode, abi.encode(name, symbol, tokens));
     }
 }
