731e8f88639837a13d0d65f5e3e27d0425f6bdb1
Chirag <chirag0264@gmail.com>
2024-08-17 14:57:56 +0530

    added

--- frontend/src/utils/constant.ts
+++ frontend/src/utils/constant.ts
@@ -4,7 +4,7 @@ export { basketFactoryContractABI } from "../abis/basketFactoryContractABI";
 export { TokenContractABI } from "../abis/TokenContractABI";
 
 export const BASKET_FACTORY_CONTRACT_ADDRESS =
-  "0xD171E0b245811dc5c51933D84E9cEE3F16A2E6F7";
+  "0xA59967531265466e762E69691E80f86943a51161";
 export const WETH_CONTRACT_ADDRESS =
   "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 export const UNISWAP_V2_CONTRACT_ADDRESS =
